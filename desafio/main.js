30f404c425f0552fb7bef6b800cc9644c5a2336d
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-10-19 08:05:47 -0300

    subindo código -fonte 19102023

---
+++ desafio/main.js
@@ -0,0 +1,253 @@
+/*main significa princiapl e mercado converncionou empbora a linguegem, aceiteua,qleur nome
+*/
+
+
+//testa se a arquvios maini está reconhecido
+
+
+//alert("Olá Mundo");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*escreve aqui abaixo é io mian da class do desafio p resto é por causa da pafgina principal dessa progrma */
+//documnets detecma a classe
+
+//ssocia aclsse a tag
+//variveis sã nomes prs objetos
+//#Faz funcionar
+//apelida os botoões
+var tecla_um=document.querySelector(".um");
+var tecla_dois=document.querySelector(".dois");
+var tecla_tres=document.querySelector(".tres");
+var tecla_quatro=document.querySelector(".quatro");
+var tecla_cinco=document.querySelector(".cinco");
+var tecla_seis=document.querySelector(".seis");
+var tecla_sete=document.querySelector(".sete");
+var tecla_oito=document.querySelector(".oito");
+var tecla_nove=document.querySelector(".nove");
+var tecla_asterisco=document.querySelector(".asterisco");
+var tecla_zero=document.querySelector(".zero");
+var tecla_hashtag=document.querySelector(".hashtag");
+//variva veis de controle da função configtecladoclass
+
+ 
+
+var tecla="";
+var  input="";
+function configteclado(input){
+    
+  //quando um botão for pressionado ele pega o texto que aparece no escreve no campo  e retorna no console  
+tecla=input.value;
+telefone.value=telefone.value+tecla
+return tecla;
+
+   // alert(tecla);
+}
+
+//#fim faz funcionar
+
+//#iniciao main principal  Javascript-web-paginas-dinamicas de Alura
+//retornadnod pro campo de telefone via id 
+
+
+/*chefe descuplpe a gambiara de reusar o mesmo arquivo */
+// o main é o jacvascript pagina principal por convensão de mercado
+
+//#requer variveis l input de desafio
+//requer tecla apelida as teclas
+var tecla_pom=document.querySelector('.tecla_pom');
+var pom_som=document.querySelector('#som_tecla_pom');
+var clap_som=document.querySelector('.tecla_clap');
+//tecla_tim
+var sons="";
+ 
+//autodetecta id
+var sonsid="";
+
+function tocarsom(input){
+//depepende de configteclado pra fucnionar por reusar varivaveis
+    //autodetecta id sem preciasar dar o coódigo converte ele em apleido direto
+sonsid="#"+ String(input.id)
+sons=document.querySelector(sonsid);
+//input é entrada acima é apelido do botão e retornara o texto aqui é apelido 
+//toca osom pelo id dele 
+sons.play()
+    
+//retorna a url apertado do som 
+tecla=sons.src
+return  tecla 
+}
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this
+
+
+
+
+
+
+function tecla9_click(){
+	
+	alert("tecla 9")
+	
+}
+
+//exivbe tela de mesagem
+//alert("teste de extensão chata ");
+//escreve data e hora naoa tela da extensão ,verifica tudinh o se ta ´pronto como horario,verificando configurações motranod -las pqra ter certeza qyue estã ook pra logar
+//document.write("altura da pagina" + innerHeight)
+//document.write("largura da pagina" + innerWidth)
+//document.getElementById("navegador").value=document.URL
+//document.getElementById("link").href="https://web.whatsapp.com/"
+ontem=new  Date();
+//s ele retornará dia  da semana de ontem então  pra saber hoje  é somar um  ,e fus ohorario tbm precisa alterar o fuso
+diasemana=ontem.getUTCDay()+1;
+
+//retorna uma hora atual
+var hora=ontem.getHours();
+//correção em 31/10/2021 rHaloween de bug pra domingo
+var diasemana_filtrado=diasemana;
+/*
+if(diasemana==1 ){
+//1 é domingo
+diasemana_filtrado="domingo";
+
+}
+if(diasemana==7 ){
+//7 é sabado
+diasemana_filtrado="sábado";
+
+}
+/*else(){
+	// pra por o dia da smena util com o numero + ª pra 2ª , 3ª ,etc
+	
+}*/
+/*
+alert("dia da semana "+ diasemana_filtrado + "ª "+ "hora"+hora);
+alert("disque 0 pra sábado;disque 1 pra domingo;disque 2 pra segunda;disque 3 pra terça;disque 4 pra quarta;disque 5 pra quinta;disque 6 pra sexta;")
+*//*
+var diadasemana=prompt("qual dia da semana hoje?",diasemana);
+var tánahora=false;
+
+
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Bitwise_XOR
+
+
+///Lógica%20de%20Programação%20Crie%20seus%20primeiros%20programas%20usando%20Javascript%20e%20HTML%20by%20Paulo%20Silveira,%20Adriano%20Almeida
+
+//https://developer.mozilla.org/pt-BR/docs/Mozilla/Add-ons/WebExtensions/Anatomy_of_a_WebExtension
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date/getUTCDay
+//Lógica%20de%20Programação%20e%20Algorítmos%20com%20JavaScript%20by%20Edécio%20Fernando%20Iepsen%20
+//https://developer.chrome.com/docs/extensions/mv3/manifest/#permissions.
+//acinma açlém de dar mesagem amostra data e hora atuais na extensão
+//abaixo atribui o valor de Pi em ve z daz letras pra substituior letra grega
+//document.getElementById("PItecla").value=Math.PI
+//jogarnsferevalor da testo mas tecla some,abaixo seta o valor do campo 
+//https://pt.stackoverflow.com/questions/188051/executar-js-em-p%C3%A1gina-de-terceiro
+//document.getElementById("campo").value=Math.PI
+
+//# se hora >19 horas eé vai dormir
+//if(){
+	
+	
+//}*/
+///*
+//PItecla.addEventListener("click",teclaconfig())
+
+//configurando teclas , Keys  sã otecla no ingls , key é tecla
+//var Key_PI=document.getElementById("PItecla")
+
+//var Key_X=document.getElementById("X")
+// a função recebe o paramentro
+//
+//configurnado teclas
+
+//mostranavegador ,document.url liberla brej pra acesso ao códgio fonte
+//#https://developer.mozilla.org/pt-BR/docs/Mozilla/Add-ons/WebExtensions/manifest.json/permissions/	
+//}
+//https://support.google.com/chrome/a/answer/2714278?hl=pt-BR
+//executando função de GPs
+
+
+
+
+
+
+
+
+///abaixo reusando opção de GPS
+ var x = document.getElementById("demo");
+            function getLocation(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(showPosition);
+                    
+                }else{
+                    x.innerHTML ="FALHOU";
+                    
+                }
+                
+            }
+        function showPosition(position){
+//latitude           
+		   x.innerHTML ="latitude   " + position.coords.latitude +
+//longitude:
+                    "longitude		" + position.coords.longitude;
+            
+        }
+/*
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+derivado do modelo https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_concat_string na web
+-->*/
+//calculador em Javascriop acima
+//https://www.guj.com.br/t/abrir-2-links-ao-mesmo-tempo/301260
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay#:~:text=O%20m%C3%A9todo%20getDay()%20retorna,onde%200%20representa%20o%20Domingo.
+//https://www.youtube.com/watch?v=iVkSlNNShsk&ab_channel=ProgramadorBR
+//configurando teclado da calculador
+//isso tlavez será minha exten~soa entçã~n~esse caso naã ofuncioanara console
+//function configteclado(){
+	//Desenvolva%20jogos%20com%20HTML5%20Canvas%20e%20JavaScript%20by%20Éderson%20Cássio%20
+	//this é este no inlês
+//document.getElementById("campo").value=this.value
+//Lógica de proagramação crie seus primeiros programas usando Javascript E HTML
+// document.getElementById
+//https://getbootstrap.com.br/docs/4.1/components/forms/ }/
+//Lógica%20de%20Programação%20Crie%20seus%20primeiros%20programas%20usando%20Javascript%20e%20HTML%20by%20Paulo%20Silveira,%20Adriano%20Almeida.pdf
+//https://odesenvolvedor.com.br/tabela-de-key-codes-para-javascript_1464.html
+//https://getbootstrap.com.br/docs/4.1/getting-started/javascript/
+
+//window.open("chrome-extension://bifmfmnipjmikbkgfhkoeahcjkfghl6hm/index.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ fim de desafio aqui é  desafio deixa pro nfinal do arquvio pra disfarçar esse reuso nos slides de apresentação senã oconfieunde o povo
+*/
+//refente a pasta desafio 
