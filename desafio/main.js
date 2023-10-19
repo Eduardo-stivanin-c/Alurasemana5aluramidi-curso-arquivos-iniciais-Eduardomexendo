c37821125dfa16b23057eefa66181f75dc57e3ad
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-10-19 10:25:37 -0300

    19102023:10:25

--- desafio/main.js
+++ desafio/main.js
@@ -1,16 +1,13 @@
 /*main significa princiapl e mercado converncionou empbora a linguegem, aceiteua,qleur nome
 */
-
-
 //testa se a arquvios maini está reconhecido
-
 //alert("Olá Mundo");
 /*escreve aqui abaixo é io mian da class do desafio p resto é por causa da pafgina principal dessa progrma */
 //documnets detecma a classe
-
 //ssocia aclsse a tag
 //variveis sã nomes prs objetos
 //#Faz funcionar
+//referente a pasta desafio com o os botões pra digitar o ocntato doa Alura
 //apelida os botoões
 var tecla_um = document.querySelector(".um");
 var tecla_dois = document.querySelector(".dois");
@@ -26,8 +23,6 @@ var tecla_zero = document.querySelector(".zero");
 var tecla_hashtag = document.querySelector(".hashtag");
 //variva veis de controle da função configtecladoclass
 
-
-
 var tecla = "";
 var input = "";
 function configteclado(input) {
@@ -39,27 +34,22 @@ function configteclado(input) {
 
     // alert(tecla);
 }
-
-//#fim faz funcionar
-
-//#iniciao main principal  Javascript-web-paginas-dinamicas de Alura
+//#fim desafio
+//#inicio main principal  Javascript-web-paginas-dinamicas de Alura
 //retornadnod pro campo de telefone via id 
-
-
 /*chefe descuplpe a gambiara de reusar o mesmo arquivo */
 // o main é o jacvascript pagina principal por convensão de mercado
-
 //#requer variveis l input de desafio
 //requer tecla apelida as teclas
+
+// inicio refeirente a pagina index.html  inicial com botãoes que fazem som
 var tecla_pom = document.querySelector('.tecla_pom');
 var pom_som = document.querySelector('#som_tecla_pom');
 var clap_som = document.querySelector('.tecla_clap');
 //tecla_tim
 var sons = "";
-
 //autodetecta id
 var sonsid = "";
-
 function tocarsom(input) {
     //depepende de configteclado pra fucnionar por reusar varivaveis
     //autodetecta id sem preciasar dar o coódigo converte ele em apleido direto
@@ -68,11 +58,15 @@ function tocarsom(input) {
     //input é entrada acima é apelido do botão e retornara o texto aqui é apelido 
     //toca osom pelo id dele 
     sons.play()
-
     //retorna a url apertado do som 
     tecla = sons.src
     return tecla
 }
+
+
+
+
+
 //retorna uma hora atual
 //correção em 31/10/2021 rHaloween de bug pra domingo
 /*referencias bibliograficas */
@@ -81,7 +75,6 @@ function tocarsom(input) {
 ///Lógica%20de%20Programação%20Crie%20seus%20primeiros%20programas%20usando%20Javascript%20e%20HTML%20by%20Paulo%20Silveira,%20Adriano%20Almeida
 //https://developer.mozilla.org/pt-BR/docs/Mozilla/Add-ons/WebExtensions/Anatomy_of_a_WebExtension
 //https://pt.stackoverflow.com/questions/188051/executar-js-em-p%C3%A1gina-de-terceiro
-
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date/getUTCDay
 //Lógica%20de%20Programação%20e%20Algorítmos%20com%20JavaScript%20by%20Edécio%20Fernando%20Iepsen%20
 //https://developer.chrome.com/docs/extensions/mv3/manifest/#permissions.
@@ -96,14 +89,13 @@ function tocarsom(input) {
 //#https://developer.mozilla.org/pt-BR/docs/Mozilla/Add-ons/WebExtensions/manifest.json/permissions/
 //https://support.google.com/chrome/a/answer/2714278?hl=pt-BR
 /*
-<!--
+
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 derivado do modelo https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_concat_string na web
--->*/
+*/
 //calculador em Javascriop acima
-
 //configurando teclado da calculador
 //isso tlavez será minha exten~soa entçã~n~esse caso naã ofuncioanara console
 //function configteclado(){
@@ -116,25 +108,7 @@ derivado do modelo https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_c
 //Lógica%20de%20Programação%20Crie%20seus%20primeiros%20programas%20usando%20Javascript%20e%20HTML%20by%20Paulo%20Silveira,%20Adriano%20Almeida.pdf
 //https://odesenvolvedor.com.br/tabela-de-key-codes-para-javascript_1464.html
 //https://getbootstrap.com.br/docs/4.1/getting-started/javascript/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
  fim de desafio aqui é  desafio deixa pro nfinal do arquvio pra disfarçar esse reuso nos slides de apresentação senã oconfieunde o povo
 */
-//refente a pasta desafio 
+//referente a pasta desafio 
